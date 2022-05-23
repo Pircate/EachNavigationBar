1cec85ddfd1ce13e09719653a197c84927babe82
LEE <18611401994@163.com>
2022-05-23 14:44:49 +0800

    优化

--- EachNavigationBar/Classes/UIViewController+Swizzling.swift
+++ EachNavigationBar/Classes/UIViewController+Swizzling.swift
@@ -1,4 +1,4 @@
-// 
+//
 //  UIViewController+Swizzling.swift
 //  EachNavigationBar
 //
@@ -65,8 +65,36 @@ extension UIViewController {
 private extension Selector {
     
     static func <=> (left: Selector, right: Selector) {
-        if let originalMethod = class_getInstanceMethod(UIViewController.self, left),
-            let swizzledMethod = class_getInstanceMethod(UIViewController.self, right) {
+        UIViewController.swizzled_method(left, right)
+    }
+}
+
+fileprivate extension NSObject {
+    
+    static func swizzled_method(_ originalSelector: Selector, _ swizzledSelector: Selector) {
+        guard
+            let originalMethod = class_getInstanceMethod(Self.self, originalSelector),
+            let swizzledMethod = class_getInstanceMethod(Self.self, swizzledSelector) else {
+            return
+        }
+        
+        // 在进行 Swizzling 的时候,需要用 class_addMethod 先进行判断一下原有类中是否有要替换方法的实现
+        let didAddMethod: Bool = class_addMethod(
+            Self.self,
+            originalSelector,
+            method_getImplementation(swizzledMethod),
+            method_getTypeEncoding(swizzledMethod)
+        )
+        // 如果 class_addMethod 返回 yes,说明当前类中没有要替换方法的实现,所以需要在父类中查找,这时候就用到 method_getImplemetation 去获取 class_getInstanceMethod 里面的方法实现,然后再进行 class_replaceMethod 来实现 Swizzing
+        if didAddMethod {
+            class_replaceMethod(
+                Self.self,
+                swizzledSelector,
+                method_getImplementation(originalMethod),
+                method_getTypeEncoding(originalMethod)
+            )
+            
+        } else {
             method_exchangeImplementations(originalMethod, swizzledMethod)
         }
     }
